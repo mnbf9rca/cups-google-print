eef4f2e04a5e6032925bb941eed54bcb83c1e8ce
mnbf9rca <mnbf9rca@users.noreply.github.com>
2017-01-01 23:08:58 +0000

    always delete /tmp/cloud-print-connector-monitor.sock

--- make-gcp-autostart.sh
+++ make-gcp-autostart.sh
@@ -3,6 +3,7 @@
 mkdir /etc/service/gcp
 cat <<'EOT' >/etc/service/gcp/run
 #!/bin/sh
+rm -f /tmp/cloud-print-connector-monitor.sock
 exec /root/go/bin/gcp-cups-connector
 EOT
 chmod +x /etc/service/gcp/run
