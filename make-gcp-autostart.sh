be01259bc52a4a842dec4f56edc07a136a09b253
mnbf9rca <mnbf9rca@users.noreply.github.com>
2017-01-02 15:44:44 +0000

    if config file exists - use it

--- make-gcp-autostart.sh
+++ make-gcp-autostart.sh
@@ -4,6 +4,11 @@ mkdir /etc/service/gcp
 cat <<'EOT' >/etc/service/gcp/run
 #!/bin/sh
 rm -f /tmp/cloud-print-connector-monitor.sock
-exec /root/go/bin/gcp-cups-connector
+if [ -r /config/gcp-cups-connector.config.json ]
+  then
+    exec /root/go/bin/gcp-cups-connector --config-filename /config/gcp-cups-connector.config.json
+  else
+    exec /root/go/bin/gcp-cups-connector
+fi
 EOT
 chmod +x /etc/service/gcp/run
